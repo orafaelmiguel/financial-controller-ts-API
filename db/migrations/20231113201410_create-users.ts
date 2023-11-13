3450ae0016b04f9a1fc38daf37d35d2355738507
Rafael Miguel <rafaelmiguel0202@gmail.com>
2023-11-13 18:30:07 -0300

    create user migrations in database

---
+++ db/migrations/20231113201410_create-users.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('users', (table) => {
+        table.uuid('id').primary()
+        table.text('name').notNullable()
+        table.text('email').notNullable()
+        table.text('session_id')
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('users')
+}
+
